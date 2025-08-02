021c0c7e565d80c90a22323527f0eeb23fa4f966
DanielChen29 <daniel.yp.chen@gmail.com>
2025-08-01 20:18:10 -0700

    Update seed.py

--- app/seed.py
+++ app/seed.py
@@ -6,6 +6,7 @@ teams = [
     {"school": "Colorado", "teamname": "Mamabird", "division": "d1m", "finish": "2", "rank": 2},
     {"school": "Oregon", "teamname": "Ego", "division": "d1m", "finish": "3T", "rank": 3},
     {"school": "Massachusetts", "teamname": "ZooDisc", "division": "d1m", "finish": "3T", "rank": 4},
+    {"school": "North Carolina", "teamname": "Darkside", "division": "d1m", "finish": "5T", "rank": 5},
 ]
 
 create_db_and_tables()
